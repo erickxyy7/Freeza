2d368829cca89ab6c1986fd413d1a791075aaa82
Erick Augusto <erickaugusto542@gmail.com>
2022-07-23 16:57:27 -0500

    update freeza.js

--- freeza.js
+++ freeza.js
@@ -1,6 +1,7 @@
 const basename = require('path').basename;
 const readFileSync = require('fs').readFileSync;
 const tokenizer = require('./tokenizer');
+const lexer = require('./lexer');
 const interpreter = require('./interpreter');
 
 var program_name = basename(process.argv[2]);
@@ -10,4 +11,5 @@ var program = readFileSync(program_name, { encoding: 'utf8', flag: 'r'});
 
 
 tokens = tokenizer(program);
+tokens = lexer(tokens);
 interpreter(tokens);
