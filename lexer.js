e64b5d3c347f4080d8059d95700dfd012d5ea385
Erick Augusto <erickaugusto542@gmail.com>
2022-07-23 18:54:50 -0500

    update

--- lexer.js
+++ lexer.js
@@ -4,9 +4,10 @@ function lexer(tokens) {
     let x, keyword, endOfKeyword;
     for(let i = 0, l = tokens.length; i < l; i++) {
         if (tokens[i] == 'if') {
+            /* Encontra o 'end' deste 'if'. */
+            
             keyword = i;
             j = i;
-            /* Encontra o 'end' deste 'if'. */
             x = 1;
             while (true) {
                 j++;
